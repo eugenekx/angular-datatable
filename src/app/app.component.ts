ad0d3990dde1307ac51d0f1f5338cbbb3fc2c3fe
eugenekx <kx.eugene@gmail.com>
2024-10-20 19:35:20 +0300

    add firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,23 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { AgGridAngular } from 'ag-grid-angular'; // Angular Data Grid Component
 import { ColDef } from 'ag-grid-community'; // Column Definition Type Interface
+
+import { inject } from '@angular/core';
+import { Firestore, collectionData, collection } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+
+interface Product {
+  id: number;
+  name: string;
+  description: string;
+  price: number;
+  stock: number;
+  category: string;
+  date: string;
+  status: 'available' | 'out of stock' | 'pending';
+  image: string;
+  rating: number;
+}
 
 @Component({
   selector: 'app-root',
@@ -19,69 +35,9 @@ import { ColDef } from 'ag-grid-community'; // Column Definition Type Interface
   `,
 })
 export class AppComponent {
-  // Row Data: The data to be displayed.
-  rowData = [
-    {
-      id: 1,
-      name: 'Laptop',
-      description: 'High-performance laptop for gaming.',
-      price: 999.99,
-      stock: 25,
-      category: 'electronics',
-      date: '2024-01-15',
-      status: 'available',
-      image: 'https://example.com/images/laptop.jpg',
-      rating: 4.5,
-    },
-    {
-      id: 2,
-      name: 'Coffee Maker',
-      description: 'Automatic coffee maker with grinder.',
-      price: 79.99,
-      stock: 50,
-      category: 'home_appliances',
-      date: '2024-02-10',
-      status: 'available',
-      image: 'https://example.com/images/coffeemaker.jpg',
-      rating: 4.0,
-    },
-    {
-      id: 3,
-      name: 'Running Shoes',
-      description: 'Lightweight running shoes for all terrains.',
-      price: 59.99,
-      stock: 100,
-      category: 'footwear',
-      date: '2024-03-05',
-      status: 'available',
-      image: 'https://example.com/images/runningshoes.jpg',
-      rating: 4.8,
-    },
-    {
-      id: 4,
-      name: 'Smartphone',
-      description: 'Latest model smartphone with advanced features.',
-      price: 799.99,
-      stock: 10,
-      category: 'electronics',
-      date: '2024-04-12',
-      status: 'available',
-      image: 'https://example.com/images/smartphone.jpg',
-      rating: 4.7,
-    },
-    {
-      id: 5,
-      name: 'Desk Chair',
-      description: 'Ergonomic office chair for comfort.',
-      price: 149.99,
-      stock: 20,
-      category: 'furniture',
-      date: '2024-05-01',
-      status: 'available',
-      image: 'https://example.com/images/deskchair.jpg',
-      rating: 4.2,
-    },
-  ];
+  product$: Observable<Product[]>;
+  firestore: Firestore = inject(Firestore);
+  rowData: Product[] = [];
 
   // Column Definitions: Defines the columns to be displayed.
   colDefs: ColDef[] = [
@@ -96,4 +52,15 @@ export class AppComponent {
     { field: 'image' },
     { field: 'rating' },
   ];
+
+  constructor() {
+    const itemCollection = collection(this.firestore, 'products');
+    this.product$ = collectionData<Product>(itemCollection);
+  }
+
+  ngOnInit() {
+    this.product$.subscribe((data) => {
+      this.rowData = data; // Обновляем данные в гриде
+    });
+  }
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,6 +4,16 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { environment } from '../environments/environment';
+
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideAnimationsAsync()]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideAnimationsAsync(),
+    provideFirebaseApp(() => initializeApp(environment)),
+    provideFirestore(() => getFirestore()),
+  ],
 };
